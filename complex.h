2940a10b31634e2e28e7568d4a497437fb5fc7c1
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-04-27 12:39:03 +0200

    Update complex.h
    Updated indents

--- complex.h
+++ complex.h
@@ -6,28 +6,28 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 class Complex{
-	private:
-		double x,y;
-	public:
-        Complex(double real);
-        Complex(double real, double imaginary);
-        static Complex fromExp(double argument, double module){
-            return Complex(module*cos(argument), module*sin(argument));
-        }
-        double real();
-        double imaginary();
-        double module();
-        double argument();
-        std::string trigonometricForm();
-        std::string exponentialForm();
+private:
+    double x,y;
+public:
+    Complex(double real);
+    Complex(double real, double imaginary);
+    static Complex fromExp(double argument, double module){
+        return Complex(module*cos(argument), module*sin(argument));
+    }
+    double real();
+    double imaginary();
+    double module();
+    double argument();
+    std::string trigonometricForm();
+    std::string exponentialForm();
 
-        Complex pow(int power);
-        friend std::ostream& operator<<(std::ostream& os, const Complex& c);
-        Complex operator+(Complex b);
-        Complex operator-(Complex b);
-        Complex operator*(Complex b);
-        Complex operator/(Complex b);
-        friend Complex operator+(double real, const Complex& complex);
-        friend Complex operator*(double real, Complex& complex);
+    Complex pow(int power);
+    friend std::ostream& operator<<(std::ostream& os, const Complex& c);
+    Complex operator+(Complex b);
+    Complex operator-(Complex b);
+    Complex operator*(Complex b);
+    Complex operator/(Complex b);
+    friend Complex operator+(double real, const Complex& complex);
+    friend Complex operator*(double real, Complex& complex);
 };
 #endif
